bf15d41fb795073d6e8f3b8bdf6b068ed7e73972
Defelo <elodef42@gmail.com>
2023-08-17 10:18:28 +0200

    Fix PremiumStatus schema

--- api/schemas/premium.py
+++ api/schemas/premium.py
@@ -9,5 +9,5 @@ class PremiumPlan(BaseModel):
 class PremiumStatus(BaseModel):
     premium: bool = Field(description="Whether premium is active for this user.")
     since: float | None = Field(description="Start timestamp of the current (contiguous) premium membership.")
-    until: float | None = Field("End timestamp of the current premium membership.")
-    autopay: str | None = Field("Premium plan used for automatic renewals.")
+    until: float | None = Field(description="End timestamp of the current premium membership.")
+    autopay: str | None = Field(description="Premium plan used for automatic renewals.")
